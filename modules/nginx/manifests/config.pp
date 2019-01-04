0227e70ea3bce651b6d6fb52c9564a4a28ad6838
PE File Sync Service <>
2019-01-04 02:29:52 +0000

    code-manager deploy signature: '7179109866e3c731402f3410338f3287df433c76'

--- modules/nginx/manifests/config.pp
+++ modules/nginx/manifests/config.pp
@@ -1,14 +1,14 @@
 class nginx::config( 
 	String $ensure     =   $::nginx::ensure,
         String $config_dir =   $::nginx::config_dir,
-        String $mode       =   $::nginx::mode,
-        String $owner      =   $::nginx::owner,
-        String $group      =   $::nginx::group,
-        String $confd      =   $::nginx::confd,
-        String $log_dir    =   $::nginx::log_dir,
-        String $pid_file   =   $::nginx::pid_file,
-Optional[String] $vdir_enable = $::nginx::vdir_enable,
-        String $process_user = $::nginx::process_user,
+        String $mode       =   $::nginx::config_mode,
+        String $owner      =   $::nginx::config_owner,
+        String $group      =   $::nginx::config_group,
+        String $confd      =   $::nginx::config_confd,
+        String $log_dir    =   $::nginx::config_log_dir,
+        String $pid_file   =   $::nginx::config_pid_file,
+Optional[String] $vdir_enable = $::nginx::config_vdir_enable,
+        String $process_user = $::nginx::conmfig_process_user,
 	String	$docroot  = $::nginx::docroot,
 ){
   file { 'nginx.conf' :
